cce9b4aed2f79814e04584c5a9dbbd4b7ac6f897
olga-smart <olgat.smart@gmail.com>
2019-08-25 17:20:26 +0700

    add content and titles to services pages

--- js/js.js
+++ js/js.js
@@ -9,12 +9,12 @@ for (let i = 0; i < priceLinkButtons.length; i++) {
 }
 
 // To open btn-link to service-page by clicking the mouse wheel
-let service_1LinkButtons = document.querySelectorAll('.btn-group .service_1-link-btn');
-for (let i = 0; i < service_1LinkButtons.length; i++) {
-    service_1LinkButtons[i].addEventListener("mousedown", function(event) {
-       if (event.which === 2) {
-           window.open('service_1.html');
-       } 
-    });
-}
+//let service_1LinkButtons = document.querySelectorAll('.btn-group .service_1-link-btn');
+//for (let i = 0; i < service_1LinkButtons.length; i++) {
+//    service_1LinkButtons[i].addEventListener("mousedown", function(event) {
+//       if (event.which === 2) {
+//           window.open('service_1.html');
+//       } 
+//    });
+//}
 
